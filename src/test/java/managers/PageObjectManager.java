af74d78adc9acf1f68f54c7c9748b79d3ef56e05
Mlulekigit config --global user.email mlu78nkosi@gmail.com <mlu78nkosi@gmail.com>
2022-08-19 17:15:08 +0200

    Logging in on the website

--- src/test/java/managers/PageObjectManager.java
+++ src/test/java/managers/PageObjectManager.java
@@ -1,7 +1,6 @@
 package managers;
 
 import org.openqa.selenium.WebDriver;
-import pages.CommonAdminPage;
 import pages.Guest.HomePage;
 import pages.LoginPage;
 
@@ -10,7 +9,7 @@ public class PageObjectManager {
     private final WebDriver driver;
     private LoginPage loginPage;
     private HomePage homePage;
-    private CommonAdminPage commonAdminPage;
+
 
     public PageObjectManager(WebDriver driver) {
         this.driver = driver;
@@ -25,9 +24,5 @@ public class PageObjectManager {
         return (homePage == null) ? homePage = new HomePage(driver) : homePage;
     }
 
-    public CommonAdminPage getCommonAdminPage() {
-
-        return (commonAdminPage == null) ? commonAdminPage = new CommonAdminPage(driver) : commonAdminPage;
-    }
 
 }
